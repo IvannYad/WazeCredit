5f37a86254f5c24ac047070c4a176b2aaa8ac497
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-07 23:06:49 +0200

    feat: set up non-DI scenario 2.0

--- WazeCredit/Controllers/HomeController.cs
+++ WazeCredit/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WazeCredit.Models;
+using WazeCredit.Models.ViewModels;
+using WazeCredit.Services;
 
 namespace WazeCredit.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel();
+            MarketForecaster marketForecaster = new MarketForecaster();
+            MarketResult currentMarket = marketForecaster.GetMarketResult();
+
+            switch (currentMarket.MarketCondition)
+            {
+                case MarketCondition.StableDown:
+                    homeViewModel.MarketForecast = "Market shows signs to go down in a stable state! It is a not a good sign to apply for credit applications! But extra credit is always piece of mind if you have handy when you need it.";
+                    break;
+                case MarketCondition.StableUp:
+                    homeViewModel.MarketForecast = "Market shows signs to go up in a stable state! It is a great sign to apply for credit applications!";
+                    break;
+                case MarketCondition.Volatile:
+                    homeViewModel.MarketForecast = "Market shows signs of volatility. In uncertain times, it is good to have credit handy if you need extra funds!";
+                    break;
+                default:
+                    homeViewModel.MarketForecast = "Apply for a credit card using our application!";
+                    break;
+            }
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
