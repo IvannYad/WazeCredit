e7b12d22733b860b7d75750ba5486441c2283813
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-07 22:56:04 +0200

    feat: set up non-DI scenario 1.0

---
+++ WazeCredit/Services/MarketForecaster.cs
@@ -0,0 +1,19 @@
+﻿using WazeCredit.Models;
+
+namespace WazeCredit.Services
+{
+    public class MarketForecaster
+    {
+        public MarketResult GetMarketResult()
+        {
+            // Call API to do some complex calculations and current stock market forecast.
+            // For now we will hardcode the result.
+            return new MarketResult() { MarketCondition = MarketCondition.StableUp };
+        }
+    }
+
+    public class MarketResult
+    {
+        public MarketCondition MarketCondition { get; set; }
+    }
+}
