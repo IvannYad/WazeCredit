51d287fd0980ade79fa0252a349db9e6c0044356
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-09 22:10:05 +0200

    feat: IOptions injection

--- WazeCredit/Controllers/HomeController.cs
+++ WazeCredit/Controllers/HomeController.cs
@@ -1,19 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Diagnostics;
 using WazeCredit.Models;
 using WazeCredit.Models.ViewModels;
 using WazeCredit.Services;
+using WazeCredit.Utility.AppSettingsClasses;
 
 namespace WazeCredit.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IMarketForecaster _marketForecaster;
+        private readonly StripeSettings _stripeOptions;
+        private readonly WazeForecastSettings _wazeForecastOptions;
+        private readonly SendGridSettings _sendGridOptions;
+        private readonly TwilioSettings _twilioOptions;
         public HomeViewModel HomeViewModel { get; set; }
-        public HomeController(IMarketForecaster marketForecaster)
+        public HomeController(IMarketForecaster marketForecaster,
+            IOptions<StripeSettings> stripeOptions,
+            IOptions<WazeForecastSettings> wazeForecastOptions,
+            IOptions<SendGridSettings> sendGridOptions,
+            IOptions<TwilioSettings> twilioOptions)
         {
             HomeViewModel = new HomeViewModel();
             _marketForecaster = marketForecaster;
+            _stripeOptions = stripeOptions.Value;
+            _wazeForecastOptions = wazeForecastOptions.Value;
+            _sendGridOptions = sendGridOptions.Value;
+            _twilioOptions = twilioOptions.Value;
 
         }
         public IActionResult Index()
@@ -38,6 +52,20 @@ namespace WazeCredit.Controllers
             }
 
             return View(homeViewModel);
+        }
+
+        public IActionResult AllConfigSettings()
+        {
+            List<string> messages = new List<string>();
+            messages.Add($"Waze config - Forecast Tracker: " + _wazeForecastOptions.ForecastTrackerEnabled);
+            messages.Add($"Stripe config - Publishable Key: " + _stripeOptions.PublishableKey);
+            messages.Add($"Stripe config - Secret Key: " + _stripeOptions.SecretKey);
+            messages.Add($"SendGrid config - SendGrid Key: " + _sendGridOptions.SendGridKey);
+            messages.Add($"Twilio config - AccountSid: " + _twilioOptions.AccountSid);
+            messages.Add($"Twilio config - Auth Token: " + _twilioOptions.AuthToken);
+            messages.Add($"Twilio config - Phone Number: " + _twilioOptions.PhoneNumber);
+
+            return View(messages);
         }
 
         public IActionResult Privacy()
